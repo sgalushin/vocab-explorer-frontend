cf89c5b8617f2d5df6acfaec0bdf2c5e4a6b5c3b
Sergei Galushin <sgalushin@et904.com>
2021-07-28 02:01:42 +0300

    Add favicon

---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,18 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <div>
+        Icons made by{" "}
+        <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">
+          Smashicons
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
