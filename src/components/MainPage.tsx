20cc08ecc24940bda4259859fe81c61753b17832
Sergei Galushin <sgalushin@et904.com>
2021-07-28 17:20:44 +0300

    Add parsing of youtube URLs (both short and full)

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,25 +1,54 @@
 import { Box, Button, TextField } from "@material-ui/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./MainPage.css";
+
+const extractVideoIdFromVideoOrUrl = (videoIdOrUrl: string): string => {
+  if (videoIdOrUrl.startsWith("https")) {
+    const url = new URL(videoIdOrUrl);
+    switch (url.hostname) {
+      case "youtube.com":
+      case "www.youtube.com":
+        return url.searchParams.get("v") ?? "";
+      case "youtu.be":
+      case "www.youtu.be":
+        return url.pathname.slice(1); // first char is "/" which we don't need
+      default:
+        return "";
+    }
+  } else {
+    return encodeURIComponent(videoIdOrUrl);
+  }
+};
+
 const MainPage = () => {
-  const [videoIdOrURL, setVideoIdOrUrl] = useState("");
+  const [videoIdOrUrl, setVideoIdOrUrl] = useState("");
+  const [goButtonDisabled, setGoButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    setGoButtonDisabled(extractVideoIdFromVideoOrUrl(videoIdOrUrl).trim() == "");
+  }, [videoIdOrUrl]);
+
   return (
-    <Box>
-      <TextField
-        style={{ width: "60%" }}
-        id="outlined-basic"
-        label="URL / Video ID"
-        variant="outlined"
-        value={videoIdOrURL}
-        onChange={(e) => setVideoIdOrUrl(e.target.value)}
-      />
-      <Link to={`/youtube/${videoIdOrURL}`}>
-        <Button variant="contained" color="primary">
-          Go
-        </Button>
-      </Link>
-    </Box>
+    <div className="video-id-and-go-button-container">
+      <div className="video-id-container">
+        <TextField
+          style={{ width: "60%" }}
+          label="URL / Video ID"
+          variant="outlined"
+          value={videoIdOrUrl}
+          onChange={(e) => setVideoIdOrUrl(e.target.value)}
+        />
+      </div>
+      <div className="go-button-container">
+        <Link to={`/youtube/${extractVideoIdFromVideoOrUrl(videoIdOrUrl)}`}>
+          <Button variant="contained" size="large" color="primary" disabled={goButtonDisabled}>
+            Go
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
