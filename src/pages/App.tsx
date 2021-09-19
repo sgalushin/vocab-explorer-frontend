1298e35f059320fe976833472d6cf96fa3db5561
Sergei Galushin <sgalushin@et904.com>
2021-09-20 00:36:32 +0300

    Page title shows the language

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import VideoPage from "./VideoPage";
 import MainPage from "./MainPage";
 import AboutPage from "./AboutPage";
 import "./App.css";
+import supportedLanguages from "../supportedLanguages";
 
 function App() {
+  useEffect( () => {
+    let langName = supportedLanguages[process.env.REACT_APP_LANGUAGE!];
+    document.title = `${langName.charAt(0).toUpperCase() + langName.slice(1)} Vocab Explorer`
+  }, [])
+
+
   return (
     <div className="App">
       <Router>
